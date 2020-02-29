e2b679a2d78d57220c2c486467ece213fdcbb06a
cheroliv <cheroliv@laposte.net>
2020-02-29 13:01:28 +0100

    first commit

---
+++ src/main/groovy/com/cheroliv/portfolio/PortfolioApplication.groovy
@@ -0,0 +1,16 @@
+package com.cheroliv.portfolio
+
+import groovy.util.logging.Slf4j
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+
+@SpringBootApplication
+class PortfolioApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/cheroliv/portfolio/PortfolioApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.cheroliv.portfolio
+
+import groovy.util.logging.Slf4j
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@Slf4j
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+        log.info('hi canary!!!!')
+    }
+
+}
